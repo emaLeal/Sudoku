e7eb542a3e44bddfbcb94a6ee12edd55fee11f2d
Leal31 <lealemanuel31@protonmail.com>
2024-04-19 09:56:40 -0500

    inicializando

---
+++ src/main/java/com/example/sudoku/controller/WelcomeController.java
@@ -0,0 +1,11 @@
+package com.example.sudoku.controller;
+
+import javafx.fxml.FXML;
+
+public class WelcomeController {
+    @FXML
+    public void initialize() {
+        System.out.println("hola"
+        );
+    }
+}
--- src/main/java/com/example/sudoku/view/WelcomeStage.java
+++ src/main/java/com/example/sudoku/view/WelcomeStage.java
@@ -1,4 +1,34 @@
 package com.example.sudoku.view;
 
-public class WelcomeStage {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeStage extends Stage {
+    public WelcomeStage() throws IOException {
+        String URL = "/com/example/sudoku/welcome-view.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(URL));
+        Parent root = loader.load();
+        setTitle("Sudoku");
+        Scene scene = new Scene(root);
+        setScene(scene);
+        setResizable(false);
+        show();
+    }
+
+    private static class WelcomeStageHolder {
+        private static WelcomeStage INSTANCE;
+    }
+
+    public static WelcomeStage getInstance() throws IOException {
+        return WelcomeStageHolder.INSTANCE = new WelcomeStage();
+    }
+
+    public static void deleteInstance() {
+        WelcomeStageHolder.INSTANCE.close();
+        WelcomeStageHolder.INSTANCE = null;
+    }
 }
