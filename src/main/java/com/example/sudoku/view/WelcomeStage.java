a0ecd0090862c4e8022ec4fa9db5cbc339c47410
cristian cobo <ccobo1405@gmail.com>
2024-04-20 15:53:47 -0500

    Interfaz de welcome-view

--- src/main/java/com/example/sudoku/view/WelcomeStage.java
+++ src/main/java/com/example/sudoku/view/WelcomeStage.java
@@ -1,4 +1,41 @@
 package com.example.sudoku.view;
 
-public class WelcomeStage {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeStage extends Stage {
+
+    public WelcomeStage() throws IOException {
+        String url = "/com/example/Sudoku/welcome-view.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(url));
+        Parent root = loader.load();
+        setTitle("Sudoku");
+        Scene scene = new Scene(root);
+        setScene(scene);
+
+        /*getIcons().add(new Image(
+                String.valueOf(getClass().getResource("/com/example/ahorcado/images/favicon.png"))));
+
+      */
+        setResizable(false);
+        show();
+    }
+
+    private static class WelcomeStageHolder {
+        private static WelcomeStage INSTANCE;
+    }
+
+    public static WelcomeStage getInstance() throws IOException {
+        return WelcomeStageHolder.INSTANCE = new WelcomeStage();
+    }
+
+    public static void deleteInstance() {
+        WelcomeStageHolder.INSTANCE.close();
+        WelcomeStageHolder.INSTANCE = null;
+    }
 }
